a4ec9433a17e09ec39f53ac71de003a38c2e80fb
BRodriguez90 <bero14@knights.ucf.edu>
2017-04-02 00:36:36 -0400

    letter animation and doc load

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,22 @@
 console.log('\'Allo \'Allo!');
+$(function(){
+   $('.about').hide();
+   $('doc-wrapper').hide();
+   $(document).click(function(){
+      audio.src = "images/letter.mp3";
+      audio.play();
+      $('.letter-top').slideToggle(1500);
+      //$('.letter-bot').slideToggle(1500);
+      $('.letter-bot').fadeOut(1600);
+
+      setTimeout(function(){
+         $('.about').fadeIn(2000);
+      },200);
+
+      setTimeout(function(){
+         $('.letter-top').remove();
+      },2000);
+      $('.doc-wrapper').fadeIn();
+   });
+
+}); // end of document ready
