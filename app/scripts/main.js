8fc22da2a875757c1c0c33f4c180c5ee0b80de32
BRodriguez90 <bero14@knights.ucf.edu>
2017-04-08 22:41:22 -0400

    update letter css

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,21 +2,22 @@ console.log('\'Allo \'Allo!');
 $(function(){
    $('.about').hide();
    $('doc-wrapper').hide();
-   $(document).click(function(){
-      audio.src = "images/letter.mp3";
+
+   $('.letter-top,.letter-bot').click(function(){ // when click on top or bottom flap of letter trigger function
       audio.play();
-      $('.letter-top').slideToggle(1500);
+      $('.letter-top').slideToggle(1200); // toggle top flap of letter
       //$('.letter-bot').slideToggle(1500);
-      $('.letter-bot').fadeOut(1600);
+      $('.letter-bot').fadeOut(1300);  // fade out bottom flap of letter
 
       setTimeout(function(){
-         $('.about').fadeIn(2000);
-      },200);
+         $('.about').fadeIn(2000); // fade in the text behind the letter
+      },400);
 
       setTimeout(function(){
-         $('.letter-top').remove();
+         $('.letter-top').remove(); // remove the top of the letter
+         $('#audio').remove(); // remove audio once "letter opens"
       },2000);
-      $('.doc-wrapper').fadeIn();
-   });
+      $('.doc-wrapper').fadeIn(); // fade in the rest of the website.
+   }); // end letter top - bot animation 
 
-}); // end of document ready
+}); // end of document ready 
